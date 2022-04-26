46cdc7bb5bac72a99706a8591f9e6a3141a8329a
Wagner Terry <goncalveswagner15@gmail.com>
2022-04-26 15:17:45 -0300

    cap 4 aula 8. Testando o estado inicial do Login

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,17 @@ import {render} from '@testing-library/react'
 import Login from './login'
 
 describe('Login Component', () => {
-    test('Should not render spinner and error on start', () => {
+    test('Should start with initial state', () => {
         const {getByTestId} = render(<Login />)
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
+        const submitButton = getByTestId('submit') as HTMLButtonElement
+        expect (submitButton.disabled).toBe(true)
+        const emailStatus = getByTestId('email-status') as HTMLButtonElement
+        expect (emailStatus.title).toBe("Campo obrigatório")
+        expect (emailStatus.textContent).toBe("🔴")
+        const passwordStatus = getByTestId('password-status') as HTMLButtonElement
+        expect (passwordStatus.title).toBe("Campo obrigatório")
+        expect (passwordStatus.textContent).toBe("🔴")
     })
 })
